e70734c0a76c202e41c40cc2ba9e3838982a7276
Rishav Choudhary <78720189+rishav015@users.noreply.github.com>
2022-10-02 00:57:19 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,16 @@
+function nav(){
+    return`<div id="nav">
+    <div id="cont">
+        <div>≡</div>
+    <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Zara_Logo.svg/800px-Zara_Logo.svg.png?20190222225032" alt="bhaiya error hai"></div>
+    </div>
+    <div id="ruby">
+        <input id="input" type="text" placeholder="SEARCH">
+        <div class="anchorRn"><a href="create account.html">lOG IN</a></div>
+        <div class="anchorRn"><a href="https://www.zara.com/in/en/help">HELP</a></div>
+        <div class="anchorRn"><a href="./search/cart.html">⌂</a></div>
+    </div>
+</div>`
+}
+
+export default nav
